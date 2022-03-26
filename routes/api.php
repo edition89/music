8aeaf6595d517cd055d8a344dfce2df7a0c9a9be
Roman <roman_gabibullae@mail.ru>
2022-03-27 01:36:03 +0300

    added api rout

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/get-song-list', [ApiController::class, 'getSongList']);
+Route::post('/play-count', [ApiController::class, 'playCount']);
+Route::post('/get-table-parameters', [ApiController::class, 'getTableParameters']);
