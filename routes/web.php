dd24f085b5757a14f33097f33df09ba5a6a52320
Roman <roman_gabibullae@mail.ru>
2022-03-27 01:35:53 +0300

    added web rout

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', function () {
+        return view('index');
+    });
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
