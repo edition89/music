ccfe34f622b26b693ccba147497a732313967117
Roman <roman_gabibullae@mail.ru>
2022-03-27 01:39:04 +0300

    added layouts

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/main.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/style.css') }}" rel="stylesheet">
+</head>
